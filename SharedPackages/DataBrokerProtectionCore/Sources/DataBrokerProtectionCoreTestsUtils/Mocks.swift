61118f43b4f3645e506dddcabf4904a30b4a7a96
Anh Do <ado@duckduckgo.com>
2025-04-28 10:09:51 -0400

    Add dbVersion param to pixels

--- SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCoreTestsUtils/Mocks.swift
+++ SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCoreTestsUtils/Mocks.swift
@@ -1352,7 +1352,7 @@ public final class MockDataBrokerOperation: DataBrokerOperation, @unchecked Send
 
     public override func main() {
         if shouldError {
-            errorDelegate?.dataBrokerOperationDidError(DataBrokerProtectionError.noActionFound, withBrokerName: nil)
+            errorDelegate?.dataBrokerOperationDidError(DataBrokerProtectionError.noActionFound, withBrokerName: nil, version: nil)
         }
 
         finish()
@@ -1396,7 +1396,7 @@ public final class MockDataBrokerOperationErrorDelegate: DataBrokerOperationErro
 
     public init() {}
 
-    public func dataBrokerOperationDidError(_ error: any Error, withBrokerName brokerName: String?) {
+    public func dataBrokerOperationDidError(_ error: any Error, withBrokerName brokerName: String?, version: String?) {
         operationErrors.append(error)
     }
 }
